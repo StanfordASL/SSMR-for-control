3935e345b4aa9acab17430aa174bec07809ff0d6
jjalora <jjalora@stanford.edu>
2022-05-26 23:10:22 -0700

    Added option to extract output data

--- utils/consolidateData.m
+++ utils/consolidateData.m
@@ -7,10 +7,13 @@ function [filePath, xData] = consolidateData(path, files, Tspan, numNodes, ...
     end
 
     xData = cell(length(files),2);
-
+    
+    % Parse inputs
     p = inputParser;
     addParameter(p, 'storeType', 'both');
+    addParameter(p, 'outputNode', 0)
     parse(p, varargin{:});
+    outputNode = p.Results.outputNode;
 
     numIterations = length(files);
     bar = ProgressBar(numIterations, 'Title', 'Load Data');
@@ -19,13 +22,18 @@ function [filePath, xData] = consolidateData(path, files, Tspan, numNodes, ...
         decay_file = append(path, currFile{:});
         qv_decay_trajectory = readmatrix(decay_file);
 
-        xData{iFile,1} = Tspan;
+        xData{iFile,1} = Tspan(1:sampleBatch:end);
         if strcmp(p.Results.storeType, 'both')
             xData{iFile,2} = qv_decay_trajectory(1:sampleBatch:end, :).';
         elseif strcmp(p.Results.storeType, 'q')
             xData{iFile,2} = qv_decay_trajectory(1:sampleBatch:end, 1:numDOF).';
-        else
+        elseif strcmp(p.Results.storeType, 'v')
             xData{iFile,2} = qv_decay_trajectory(1:sampleBatch:end, numDOF+1:end).';
+        elseif strcmp(p.Results.storeType, 'output')
+            qout = qv_decay_trajectory(1:sampleBatch:end, 3*outputNode+1:3*outputNode+3);
+            vout = qv_decay_trajectory(1:sampleBatch:end, (numDOF + 3*outputNode+1):(numDOF + 3*outputNode+3));
+            y_out = cat(2, qout, vout);
+            xData{iFile,2} = y_out.';
         end
 
         bar(1, [], []);
@@ -39,8 +47,11 @@ function [filePath, xData] = consolidateData(path, files, Tspan, numNodes, ...
     elseif strcmp(p.Results.storeType, 'q')
         filePath = append(path, 'output/', 'qConsolidatedData');
         save(filePath,'xData');
-    else
+    elseif strcmp(p.Results.storeType, 'v')
         filePath = append(path, 'output/', 'vConsolidatedData');
+        save(filePath,'xData');
+    elseif strcmp(p.Results.storeType, 'output')
+        filePath = append(path, 'output/', 'qv_out_ConsolidatedData');
         save(filePath,'xData');
     end
 
